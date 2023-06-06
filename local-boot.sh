0ac6eab9a90068c39c09ead079ea8044c1adfca2
Michael <93428701+0xDeOP@users.noreply.github.com>
2023-06-06 12:55:22 +0700

    refactor: masked server IP

--- local-boot.sh
+++ local-boot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bitcoind -printtoconsole=1 --regtest=1 --server=1 -connect=116.203.193.164:18444 -txindex=1 -rpcuser=deop-regtest -rpcpassword=deop-regtest -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcport=18443 -fallbackfee=0.00001
+bitcoind -printtoconsole=1 --regtest=1 --server=1 -connect=${SERVER_IP}:18444 -txindex=1 -rpcuser=deop-regtest -rpcpassword=deop-regtest -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcport=18443 -fallbackfee=0.00001
