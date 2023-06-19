95ee031680c65c4c20745dcc6301d46caf7a413f
Michael <93428701+0xDeOP@users.noreply.github.com>
2023-06-19 06:01:31 +0000

    feat: enabled scan indexer

--- ordinals-indexer/start_chainhook_scan.sh
+++ ordinals-indexer/start_chainhook_scan.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-chainhook predicates scan ./ordinals-indexer/ordinals.chainhook.json --config-path=./ordinals-indexer/chainhook.toml
+while true; do chainhook predicates scan ./ordinals-indexer/ordinals.chainhook.json --config-path=./ordinals-indexer/chainhook.toml; sleep 60; done
--- start_indexer.sh
+++ start_indexer.sh
@@ -5,3 +5,4 @@ cd ./ordinals-api/
 pm2 start -f dist/src/index.js
 
 cd ../
+pm2 start -f ./ordinals-indexer/start_chainhook_scan.sh
