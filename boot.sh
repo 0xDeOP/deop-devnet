c6a60445e0ca31c3e72202fd21a108580164fd0a
Michael <93428701+0xDeOP@users.noreply.github.com>
2023-06-19 05:23:22 +0000

    feat: enabled zmq

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bitcoind -printtoconsole=1 -txindex=1 -regtest=1 -rpcuser=deop-regtest -rpcpassword=deop-regtest -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcport=18443 -fallbackfee=0.00001 &
+bitcoind -zmqpubhashtx="tcp://0.0.0.0:29000" -zmqpubrawblock="tcp://0.0.0.0:29000" -zmqpubhashblock="tcp://0.0.0.0:29000" -zmqpubrawtx="tcp://0.0.0.0:29000" -zmqpubhashtxhwm=10000 -printtoconsole=1 -txindex=1 -regtest=1 -rpcuser=deop-regtest -rpcpassword=deop-regtest -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcport=18443 -fallbackfee=0.00001 &
 
 echo "Booting bitcoind ..."
 
