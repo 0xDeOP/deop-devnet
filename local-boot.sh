b18e3c9405bcbfcbd80556f5481b105409b2b8a0
Michael <93428701+0xDeOP@users.noreply.github.com>
2023-06-19 05:44:52 +0000

    feat: changed zeromq endpoint

--- local-boot.sh
+++ local-boot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bitcoind -zmqpubhashtx="tcp://0.0.0.0:29000" -zmqpubrawblock="tcp://0.0.0.0:29000" -zmqpubhashblock="tcp://0.0.0.0:29000" -zmqpubrawtx="tcp://0.0.0.0:29000" -zmqpubhashtxhwm=10000 -printtoconsole=1 --regtest=1 --server=1 -connect=${SERVER_IP}:18444 -txindex=1 -rpcuser=deop-regtest -rpcpassword=deop-regtest -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcport=18443 -fallbackfee=0.00001
+bitcoind -zmqpubhashblock="tcp://0.0.0.0:29000" -zmqpubrawblock="tcp://0.0.0.0:29001" -zmqpubhashtx="tcp://0.0.0.0:29002"  -zmqpubrawtx="tcp://0.0.0.0:29003" -zmqpubhashtxhwm=10000 -printtoconsole=1 --regtest=1 --server=1 -connect=${SERVER_IP}:18444 -txindex=1 -rpcuser=deop-regtest -rpcpassword=deop-regtest -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcport=18443 -fallbackfee=0.00001
